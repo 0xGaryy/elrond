dcfba8f973b566e85fa034d2e47d21301a4d49fe
GARYYY <gary.aflalo@epitech.eu>
2022-04-27 17:21:36 +0200

    ok

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,25 @@
 import type { NextPage } from 'next';
-import { Box } from '@chakra-ui/react';
+import { Box, Show } from '@chakra-ui/react';
 import { MainLayout } from '../components/MainLayout';
 import { HeaderMenu } from '../components/HeaderMenu';
-import { Hero } from '../components/Hero';
 import { HeaderMenuButtons } from '../components/HeaderMenuButtons';
+import { MintHero } from '../components/MintHero';
 import { HeroImage } from '../components/HeroImage';
-import { Faq } from '../components/Faq';
-import { Roadmap } from '../components/Roadmap';
-import { Team } from '../components/Team';
 
 const Home: NextPage = () => {
   return (
     <MainLayout>
       <HeaderMenu>
-        <HeaderMenuButtons enabled={['auth', 'mint', 'about']} />
+        <HeaderMenuButtons enabled={['auth', 'about']} />
       </HeaderMenu>
       <Box
         display="flex"
         justifyContent="space-between"
         mt={{ base: 8, xl: 12, '2xl': 24 }}
       >
-        <Hero />
+        <MintHero />
         <HeroImage />
       </Box>
-      <Faq />
-      <Roadmap />
-      <Team />
     </MainLayout>
   );
 };
