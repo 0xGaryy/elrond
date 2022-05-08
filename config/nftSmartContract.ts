794bc31859961150eb252e6b5023428f94745f16
GARYYY <gary.aflalo@epitech.eu>
2022-05-08 21:18:21 +0200

    ok

--- config/nftSmartContract.ts
+++ config/nftSmartContract.ts
@@ -25,7 +25,7 @@ export const isDropActive = false;
 export const tokensLimitPerAddressPerDrop = 0;
 
 // Enable/Disable allowlist. UI will react on this. Remember to keep it in in sync with Smart Contract. This is very important.
-export const isAllowlistEnabled = true;
+export const isAllowlistEnabled = false;
 
 // Base transaction gas limit for the mint tx, it will be calculated, this is just a base
 export const mintTxBaseGasLimit = 14000000;
